501625f995a9871ea3df83dc5a6103436d8b9613
Travis Friesen <rexstuff@gmail.com>
2017-03-29 10:19:48 -0500

    Cleanup and added seasons

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # password-vortex
-A python tool for generating passwords commonly used when password rotation is enforced
+A python tool for generating passwords commonly used when password rotation is enforced.
 
 Enforcing password rotation is bad. Users (myself included) are lazy, and when forced to regularly update their passwords will
 fall back on common, predictable themes, usually centered around the current date.
@@ -10,16 +10,16 @@ For example, a user updating his or her password every month might use the follo
 January2017!
 February2017!
 March2017!
-etc
+...
 ```
 
 If updating every 4 months, a user might employ the seasons:
 
 ```
-Fall16
-Winter16
-Spring17
-etc
+Fall_16
+Winter_16
+Spring_17
+...
 ```
 
 Basically I got tired of generating or updating new password lists for every pen test, and so: password-vortex!
@@ -29,10 +29,12 @@ Basically I got tired of generating or updating new password lists for every pen
 Prints to standard out, one password per line.
 
 ```bash
-python vortex.py [options]
+vortex.py [options]
 ```
 
 ## Options
+
+**-h** help
 
 **-m** \<min length\> (default: 8)
 
@@ -42,49 +44,75 @@ python vortex.py [options]
 
 **-l** \<months\>, number of months to go back (default: 6)
 
-**-H** include major holidays (ie Christmas, Easter) that occur during the time period (coming)
+**-H** include major holidays (ie Christmas, Easter) that occur during the time period (coming) (default: off)
 
-**-s** \<special chars\>, special characters to append to the end of passwords, to generate more candidates. Use 's' for space. Only a single character is appended at a time. '!' and '.' are good candidates (default: none)
+**-e** \<chars\>, characters to append to the **e**nd of passwords, to generate more candidates. Use 's' for space. Only a single character is appended at a time. '!' and '.' are good candidates (default: none)
+
+**-s** \<chars\>, characters to insert as a **s**eparator between the month or season and the year. Use 's' for space. Only a single character is appended at a time. '!', '.', ',', '@', '#' and '_' are all good candidates (default: none). Vortex currently (and intentionally) only does one end character, OR one separator character per password candidate.
 
 **-c** \<complexity\>, minimum password complexity allowed. In other words, a generated password must have this number of characters from different sets (upper, lower, number, etc). Microsoft environments generally require 3. (default: 2)
 
+**-x** include common misspellings (ie Febuary, Autum) (coming) (default: off)
 
 
 ## Examples
 
-```./vortex.py -m8 -d 2015-10 -l 2 -s ! -c 3 -H```
+```vortex.py -m 8 -d 2015-02 -l 2 -s . -c 3 -e !```
 
-Should generate (be sure to escape that '!')
+Will generate (careful with that "!" in bash)
 
 ```
-sep2015!
-Sep2015!
-september2015!
-september15!
-September2015
-September2015!
-September15
-September15!
-oct2015!
-Oct2015!
-october2015!
-october2015!
-October2015
-October2015!
-halloween2015!
-halloween15!
-Halloween15!
-Halloween2015
-Halloween2015
-Halloween2015!
-Fall2015
-fall2015!
-Autumn15
-Autumn15!
-autumn15!
-Autumn2015
-Autumn2015!
-autumn2015!
+feb2015!
+feb.2015
+Feb2015!
+Feb.2015
+february15!
+february.15
+February15
+February15!
+February.15
+february2015!
+february.2015
+February2015
+February2015!
+February.2015
+jan2015!
+jan.2015
+Jan2015!
+Jan.2015
+january15!
+january.15
+January15
+January15!
+January.15
+january2015!
+january.2015
+January2015
+January2015!
+January.2015
 ```
 
-Plus maybe Canadian thanksgiving. Also, this list was manually generated, so I may have missed one and the order is wrong.
+
+## FAQ
+
+### Any tips for using this tool?
+
+If you can afford extra password guesses, go back further than you think you need to. User accounts are frequently abandoned (ie an employee leaves) and not properly disabled. On my own pentests I've found accounts using passwords that would seem to be 3+ years old, despite much shorter expiration policies.
+
+### Why don't you include passwords where the year comes first?
+
+Because I have never ever ever once seen a password in that format. People just don't think 2015March, it's always March2015.
+
+### Why don't allow multiple end characters to be appended or as separators?
+
+While you will occasionally see a user use two special characters at the end (ie March2015!!), it's pretty rare, and the point of this tool is to create passwords that are very commonly used.
+
+### Why don't password candidates get separator AND suffix characters? Why only one or the other?
+
+See previous answer.
+
+### Who decides what counts as a 'major' Holiday?
+
+I do.
+
+
--- vortex.py
+++ vortex.py
@@ -7,6 +7,33 @@ from datetime import date
 help_string = """Password-vortex!
 
 Generate passwords commonly used in password rotation environments.
+
+Options:
+
+\t-h\t help
+
+\t-m\t <min length> (default: 8)
+
+\t-M\t <max length> (default: 100)
+
+\t-d\t <start date>, in format YYYY-MM (default: today)
+
+\t-l\t <months>, number of months to go back (default: 6)
+
+\t-H\t include major holidays (ie Christmas, Easter) that occur during the time period (coming) (default: off)
+
+\t-e\t <chars>, characters to append to the End of passwords, to generate more candidates. Use 's' for space. Only a single character 
+\t\t is appended at a time. '!' and '.' are good candidates (default: none)
+
+\t-s\t <chars>, characters to insert as a Separator between the month or season and the year. Use 's' for space. Only a single character 
+\t\t is appended at a time. '!', '.', ',', '@', '#' and '_' are all good candidates (default: none). Vortex currently (and intentionally) 
+\t\t only does one end character, OR one separator character per password candidate.
+
+\t-c\t <complexity>, minimum password complexity allowed. In other words, a generated password must have this number of characters from 
+\t\t different sets (upper, lower, number, etc). Microsoft environments generally require 3. (default: 2)
+
+\t-x\t include common misspellings (ie Febuary, Autum) (coming) (default: off)
+
 """
 
 
@@ -24,6 +51,21 @@ month_list = [
     "november",
     "december" ]
 
+
+seasons_list = [
+    "winter",
+    "winter",
+    ["winter", "spring"],
+    "spring",
+    "spring",
+    ["spring", "summer"],
+    "summer",
+    "summer",
+    ["summer", "autumn", "fall"],
+    ["autumn", "fall"],
+    ["autumn", "fall"],
+    ["autumn", "fall", "winter"] ]
+    
 
 
 def checkLen(passwd, min_len, max_len):
@@ -52,7 +94,23 @@ def getDate(mydate):
     return int(dates[0]), int(dates[1])
 
 
-def makeCandidates(start_year, start_month, num_months, special_chars, use_holidays, use_misspellings):
+def formatCandidate(start, end, suffix_chars, separator_chars):
+    candidates = []
+    candidates.append(start + end)
+    
+    if len(suffix_chars) > 0:
+        for char in suffix_chars:
+            candidates.append(start+end+char)
+    if len(separator_chars) > 0:
+        for char in separator_chars:
+            candidates.append(start+char+end)
+            
+    candidates.extend(map(str.capitalize, candidates))
+    
+    return candidates
+
+
+def makeCandidates(start_year, start_month, num_months, suffix_chars, separator_chars, use_holidays, use_misspellings):
     candidates = []
     cur_month = start_month -1
 
@@ -62,26 +120,22 @@ def makeCandidates(start_year, start_month, num_months, special_chars, use_holid
             #TODO: check for overflow (negative years)
             start_year = start_year - 1
             cur_month = 11
-        cur_candidates.append(month_list[cur_month] + str(start_year))
-        cur_candidates.append(month_list[cur_month] + str(start_year)[-2:])
-        #include short month format (feb, mar, etc)
-        cur_candidates.append(month_list[cur_month][:3] + str(start_year))
-        cur_candidates.append(month_list[cur_month][:3] + str(start_year)[-2:])
+            
+        cur_candidates.extend(formatCandidate(month_list[cur_month][:3], str(start_year)[-2:], suffix_chars, separator_chars))
+        cur_candidates.extend(formatCandidate(month_list[cur_month][:3], str(start_year), suffix_chars, separator_chars))
+        cur_candidates.extend(formatCandidate(month_list[cur_month], str(start_year)[-2:], suffix_chars, separator_chars))
+        cur_candidates.extend(formatCandidate(month_list[cur_month], str(start_year), suffix_chars, separator_chars))
         
-        candidates.extend(cur_candidates)
-        candidates.extend(map(str.capitalize, cur_candidates))
+        #add seasons. Will add duplicate entries, which are later removed
+        for season in seasons_list[cur_month]:
+            cur_candidates.extend(formatCandidate(season, str(start_year), suffix_chars, separator_chars))
+            cur_candidates.extend(formatCandidate(season, str(start_year)[-2:], suffix_chars, separator_chars))
         
         num_months -= 1
         cur_month -= 1
+        
+        candidates.extend(cur_candidates)
 
-    
-    cur_candidates = []
-    if len(special_chars) > 0:
-        for candidate in candidates:
-            for char in special_chars:
-                cur_candidates.append(candidate + char)
-    
-    candidates.extend(cur_candidates)
     
     return candidates
 
@@ -93,7 +147,10 @@ def trimCandidates(candidates, min_len, max_len, min_complexity):
         if checkLen(candidate, min_len, max_len) and checkComplexity (candidate, min_complexity):
             new_candidates.append(candidate)
     
-    return new_candidates
+    #remove any duplicates before returning, https://www.peterbe.com/plog/uniqifiers-benchmark (comments)
+    seen = set()
+    seen_add = seen.add
+    return [x for x in new_candidates if not (x in seen or seen_add(x))]
     
 
 def main(argv):
@@ -102,7 +159,8 @@ def main(argv):
     max_len = 100
     num_months = 6
     use_holidays = False
-    special_chars = ""
+    suffix_chars = ""
+    separator_chars = ""
     complexity = 2
     use_misspellings = False
     
@@ -110,7 +168,7 @@ def main(argv):
     start_month = date.today().month
 
     try:
-        opts, args = getopt.getopt(argv,"hm:M:d:l:Hs:c:x")
+        opts, args = getopt.getopt(argv,"hm:M:d:l:Hs:e:c:x")
 
     except getopt.GetoptError:
         print "Error in options.\n"
@@ -134,10 +192,15 @@ def main(argv):
             print "Holidays not currently implemented, sorry"
             sys.exit(2)
         elif opt == "-s":
-            special_chars = list(arg)
+            separator_chars = list(arg)
             #convert 's' to a space
-            if "s" in special_chars:
-                special_chars[special_chars.index("s")] = " "
+            if "s" in separator_chars:
+                separator_chars[separator_chars.index("s")] = " "
+        elif opt == "-e":
+            suffix_chars = list(arg)
+            #convert 's' to a space
+            if "s" in suffix_chars:
+                suffix_chars[suffix_chars.index("s")] = " "
         elif opt == "-c":
             complexity = int(arg)
         elif opt == "-x":
@@ -150,11 +213,12 @@ def main(argv):
             sys.exit(2)
             
 
-    candidates = makeCandidates(start_year, start_month, num_months, special_chars, use_holidays, use_misspellings)
+    candidates = makeCandidates(start_year, start_month, num_months, suffix_chars, separator_chars, use_holidays, use_misspellings)
     candidates = trimCandidates(candidates, min_len, max_len, complexity)
     
     for candidate in candidates:
         print candidate
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
